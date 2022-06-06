4f3379370a856dc59529eb1427c1a438dca4ff4f
eslam awad <awadeslam825@gmail.com>
2022-06-06 20:35:07 +0200

    Adding UI For Login And Register

---
+++ Ostaz/Controllers/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ostaz
+//
+//  Created by eslam awad elsayed awad on 06/06/2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- Ostaz/Controllers/RegisterViewController.swift
+++ Ostaz/Controllers/RegisterViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RegisterViewController.swift
 //  Ostaz
 //
 //  Created by eslam awad elsayed awad on 06/06/2022.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RegisterViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
-
 }
-
