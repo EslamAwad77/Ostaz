a085b639d8f4f0312a6d55db76280f5e8f9726bd
eslam awad <awadeslam825@gmail.com>
2022-06-06 18:52:08 +0200

    Initial Commit

---
+++ Ostaz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ostaz
+//
+//  Created by eslam awad elsayed awad on 06/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
