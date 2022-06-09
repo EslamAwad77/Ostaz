6b3eb326b8e9969dd247c0b68a849356b471e47a
eslam awad <awadeslam825@gmail.com>
2022-06-09 16:22:32 +0200

    Adding some of UI in Home Body

---
+++ Ostaz/Controllers/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  Ostaz
+//
+//  Created by eslam awad elsayed awad on 09/06/2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ Ostaz/Controllers/LocationViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LocationViewController.swift
+//  Ostaz
+//
+//  Created by eslam awad elsayed awad on 07/06/2022.
+//
+
+import UIKit
+
+class LocationViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
