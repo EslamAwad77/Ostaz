59641d2700140cfed0cf3f4f2fe61dff26876ab4
eslam awad <awadeslam825@gmail.com>
2022-06-14 01:38:07 +0200

    Adding DropDown Documentation and UI

--- Ostaz/Controllers/LocationViewController.swift
+++ Ostaz/Controllers/LocationViewController.swift
@@ -6,11 +6,60 @@
 //
 
 import UIKit
+import DropDown
 
-class LocationViewController: UIViewController {
-
+class LocationViewController: UIViewController{
+    
+    //-------------------Variables------------------------
+    let dropDown = DropDown()
+    let cityValues: [String] = ["alex", "mans"]
+    let areaValues: [String] = ["alex library", "samia elgamal"]
+    
+    //-------------------Outlet---------------------------
+    @IBOutlet weak var viewCity: UIView!
+    @IBOutlet weak var viewArea: UIView!
+    @IBOutlet weak var lblCity: UILabel!
+    @IBOutlet weak var lblArea: UILabel!
+    
+    //-------------------Actions--------------------------
+    
+    @IBAction func btnSelectCity(_ sender: UIButton) {
+        dropDown.show()
+    }
+    
+    @IBAction func btnSelectArea(_ sender: UIButton) {
+        dropDown.show()
+    }
+    
+    @IBAction func btnConfirmLocation(_ sender: UIButton) {
+        
+    }
+    
     //-------------------LifeCycle------------------------
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpUI()
+    }
+    
+    //-------------------functions------------------------
+    func setUpUI(){
+        
+        
+        
+        dropDown.anchorView = viewCity
+        dropDown.dataSource = cityValues
+        dropDown.anchorView = viewArea
+        dropDown.dataSource = areaValues
+        dropDown.bottomOffset = CGPoint(x: 0, y:(dropDown.anchorView?.plainView.bounds.height)!)
+        dropDown.topOffset = CGPoint(x: 0, y:-(dropDown.anchorView?.plainView.bounds.height)!)
+        dropDown.direction = .any
+        dropDown.selectionAction = { [unowned self] (index: Int, item: String) in
+            print("Selected item: \(item) at index: \(index)")
+            self.lblCity.text = cityValues[index]  // put the itemSelected in the lbl.txt
+            self.lblArea.text = areaValues[index] // error he put the next area from self
+        }
     }
 }
+
+
