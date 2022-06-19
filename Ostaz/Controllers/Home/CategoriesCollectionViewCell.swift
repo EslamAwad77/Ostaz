a2a9e02f7ad69d7f8e9723171171286c0070658d
eslam awad <awadeslam825@gmail.com>
2022-06-19 21:47:53 +0200

    Starting of category UI

---
+++ Ostaz/Controllers/Home/CategoriesCollectionViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  CategoriesCollectionViewCell.swift
+//  Ostaz
+//
+//  Created by eslam awad elsayed awad on 19/06/2022.
+//
+
+import UIKit
+
+class CategoriesCollectionViewCell: UICollectionViewCell {
+    //-------------------IBOutlet------------------------
+    
+    
+    
+    //-------------------Variables------------------------
+    
+    static let identifier = String(describing: CategoriesCollectionViewCell.self)
+    
+    //-------------------Functions------------------------
+    
+//    func setUp(_ slide: CollectionViewSlide){
+//
+//        slideImgViewDescription.image = slide.descriptionImage
+//        slideImgViewTeacher.image = slide.teacherImage
+//        lblTeacherName.text = slide.teacherName
+//        lblTeacherJob.text = slide.teacherJob
+//    }
+}
